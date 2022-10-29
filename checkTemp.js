d4e327c08aceeedadf3e60930ade61393770fce6
MartaPiirikivi <115346427+MartaPiirikivi@users.noreply.github.com>
2022-10-29 09:23:47 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,14 @@
+function checkTemp(t) {
+if (t > 0 && t <= 20) {
+    console.log("-1")
+}
+else if (t > 21 && t <= 40 ) {
+        console.log("0");
+}
+else if (t > 41 && t <= 60 ) {
+            console.log("1")
+}
+else {
+    console.log("FALSE")
+}
+}
