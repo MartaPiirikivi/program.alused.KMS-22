c9f6bc2693dec34cfc2bd09761fdbd5408232c9d
MartaPiirikivi <115346427+MartaPiirikivi@users.noreply.github.com>
2022-10-29 09:45:58 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -3,11 +3,11 @@ if (t > 0 && t <= 20) {
     console.log("-1")
     return -1
 }
-else if (t > 21 && t <= 40 ) {
+else if (t >= 21 && t <= 40 ) {
         console.log("0");
         return 0
 }
-else if (t > 41 && t <= 60 ) {
+else if (t >= 41 && t <= 60 ) {
             console.log("1")
         return 1
 }
