e55ba0882ca87e59f2b91aad024fb19b7ef405dd
MartaPiirikivi <115346427+MartaPiirikivi@users.noreply.github.com>
2022-10-29 09:44:54 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,13 +1,13 @@
 function checkTemp(t) {
-if (t => 0 && t <= 20) {
+if (t > 0 && t <= 20) {
     console.log("-1")
     return -1
 }
-else if (t => 21 && t <= 40 ) {
+else if (t > 21 && t <= 40 ) {
         console.log("0");
         return 0
 }
-else if (t => 41 && t <= 60 ) {
+else if (t > 41 && t <= 60 ) {
             console.log("1")
         return 1
 }
